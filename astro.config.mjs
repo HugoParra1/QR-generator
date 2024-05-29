66ca4cf2f43fa236bd0ad81becf73e31b791acdb
HugoParra1 <hugo.pargon@gmail.com>
2024-05-29 10:15:21 +0200

    Setup for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import netlify from "@astrojs/netlify";
-
+import vercel from "@astrojs/vercel";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
   output: "server",
-  adapter: netlify()
+  adapter: vercel()
 });
